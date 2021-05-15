3da4699efebe87502f95c626c0dbf78cec329832
papillon129245 <marfro245@gmail.com>
2021-05-15 02:36:05 +0000

    ADD: redux for cars, finished almost

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,138 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import { selectCars } from '../features/car/carSlice';
+import { useSelector } from 'react-redux';
 
 function Header() {
+
+    const [burgerStatus, setBurgerStatus] = useState(false);
+    const cars = useSelector(selectCars);
+
     return (
-        <div>
+        <Container>
+            <a>
+                <img src="../images/logo.svg" alt="" />
+            </a>
+            <Menu>
+                {cars && cars.map((car, index) =>(
+                    <a key={index} href="#">{car}</a>
+                ))}
+                {/* <a href="#">Model S</a>
+                <a href="#">Model 3</a>
+                <a href="#">Model Y</a>
+                <a href="#">Model X</a> */}
+            </Menu>
+            <RightMenu>
+                <a href="#">SHOP</a>
+                <a href="#">TESLA Account</a>
+                <BurgerMenu onClick={() => setBurgerStatus(true)} />
+            </RightMenu>
+            <BurgerNavContent show={burgerStatus}>
+                <Wrapper>
+                    <CloseIconX onClick={() => setBurgerStatus(false)} />
+                </Wrapper>
+                {cars && cars.map((car, index) =>(
+                    <li key={index} ><a href="#">{car}</a></li>
+                ))}
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Inventory</a></li>
+                <li><a href="#">Trade-In</a></li>
+                <li><a href="#">Cyber Truck</a></li>
+                <li><a href="#">Roadster</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+
+            </BurgerNavContent>
             
-        </div>
+            
+            
+        </Container>
     )
 }
 
 export default Header;
+
+const Container = styled.div`
+    min-height: 60px;
+    position: fixed;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 1;
+`
+
+const Menu = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+
+    > a {
+        font-weight: 600;
+        text-transform: uppercase;
+        padding: 0 10px;
+        flex-wrap: nowrap;
+
+    }
+
+    @media(max-width: 768px) {
+        display: none;
+    }
+`
+
+const RightMenu = styled.div`
+    display: flex;
+    align-items: center;
+      > a {
+        font-weight: 600;
+        text-transform: uppercase;
+        margin-right: 10px;
+
+    }
+`
+const BurgerMenu = styled(MenuIcon)`
+    cursor: pointer;
+
+
+`
+const BurgerNavContent =styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background-color: white;
+    opacity: 0.9;
+    width: 300px;
+    z-index: 16;
+    list-style: none;
+    padding: 20px;
+    transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+    transition: transform 0.3s ease-in-out;
+
+
+    > li {
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+
+        > a {
+            font-weight: 600;
+        }
+    }
+`
+const CloseIconX =styled(CloseIcon)`
+
+    cursor: pointer;
+`
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
+import Fade from 'react-reveal/Fade';
 
 function Section({ title, description, backgroundImg, leftBtnText, rightBtnText }) {
     
     return (
         <Wrap bgImage={backgroundImg}>
-            <ItemText>
-                <h1>{title}</h1>
-                <p>{description}</p>
-            </ItemText>
+            <Fade bottom>
+                <ItemText>
+                    <h1>{title}</h1>
+                    <p>{description}</p>
+                </ItemText>
+            </Fade>
             <Buttons>
-                <ButtonGroup>
-                    <LeftButton>{leftBtnText}</LeftButton>
-                    { rightBtnText && 
-                        <RightButton>{rightBtnText}</RightButton>
-                    }
-                </ButtonGroup>
-                <DownArrow src="../images/down-arrow.svg" />
+                <Fade bottom>
+                    <ButtonGroup>
+                        <LeftButton>{leftBtnText}</LeftButton>
+                        { rightBtnText && 
+                            <RightButton>{rightBtnText}</RightButton>
+                        }
+                    </ButtonGroup>
+                </Fade>
+                    <DownArrow src="../images/down-arrow.svg" />
+                
             </Buttons>           
         </Wrap>
     )
@@ -40,6 +46,10 @@ const Wrap = styled.div`
 const ItemText = styled.div`
     padding-top: 15vh;
     text-align: center;
+
+    > h1 {
+        margin-bottom: 10px;
+    }
 `
 const ButtonGroup = styled.div`
     display: flex;
